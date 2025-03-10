9a48ea3e8f9fd3216f0d11a53e1903a2ae1c6ade
Ahmed Eldaby <ahmed202124199@gmail.com>
2025-03-11 01:58:56 +0200

    MainActivity.kt

--- app/src/main/java/com/example/whatnow/MainActivity.kt
+++ app/src/main/java/com/example/whatnow/MainActivity.kt
@@ -2,77 +2,107 @@ package com.example.whatnow
 
 import android.os.Bundle
 import android.util.Log
-import androidx.activity.enableEdgeToEdge
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.isVisible
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.whatnow.databinding.ActivityMainBinding
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-
 class MainActivity : AppCompatActivity() {
 
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var newsAdapter: NewsAdapter
+    private lateinit var categoriesAdapter: CategoriesAdapter
+    private lateinit var newsCallable: NewsCallable
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-        loadNews()
 
-        binding.swipeRef.setOnRefreshListener { loadNews() }
-    }
-
-    private fun loadNews() {
-        val interceptor: HttpLoggingInterceptor = HttpLoggingInterceptor()
-        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
-        val client = OkHttpClient.Builder().addInterceptor(interceptor).build()
-
-        val retrofit = Retrofit
-            .Builder()
+        val retrofit = Retrofit.Builder()
             .baseUrl("https://newsapi.org")
-            .client(client)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
+        newsCallable = retrofit.create(NewsCallable::class.java)
 
-        val c = retrofit.create(NewsCallable::class.java)
-        c.getNews().enqueue(object : Callback<News> {
-            override fun onResponse(p0: Call<News>, p1: Response<News>) {
-                val news = p1.body()
-                val articles = news?.articles!!
+        setupAdapters()
 
-               articles.removeAll{it.title == "[Removed"}
+        fetchNews(null)
+        val categories = mutableListOf(
+            Category(
+                R.drawable.entertainment, "Entertainment", "entertainment"),
+            Category(R.drawable.business, "Business", "business"),
+            Category(R.drawable.technology, "Technology", "technology"),
+            Category(R.drawable.health, "Health", "health"),
+            Category(R.drawable.football_icon, "Sports", "sports")
+        )
+        categoryNews(categories)
 
-                Log.d("trace", "Articls: $articles")
-                showNews(articles)
-                binding.progressBar.isVisible=false
-                binding.swipeRef.isRefreshing=false
+
+        binding.swipeRef.setOnRefreshListener { fetchNews(null) }
+    }
+
+
+    private fun fetchNews(category: String? = null) {
+        binding.progressBar.isVisible = true
+
+        newsCallable.getNews(category).enqueue(object : Callback<News> {
+            override fun onResponse(call: Call<News>, response: Response<News>) {
+                binding.progressBar.isVisible = false
+                binding.swipeRef.isRefreshing = false
+
+                if (!response.isSuccessful || response.body() == null || response.body()?.articles.isNullOrEmpty()) {
+                    Log.e("MainActivity", "Response unsuccessful or articles are null")
+                    return
+                }
+
+                val articles = response.body()!!.articles.toMutableList()
+                articles.removeAll { it.title == "[Removed]" || it.urlToImage == null }
+
+                Log.d("trace", "Data: $articles")
+                newsAdapter.updateNews(articles)
+                binding.categoriesList.visibility = View.VISIBLE
             }
 
-            override fun onFailure(p0: Call<News>, p1: Throwable) {
-                Log.d("trace", "Error: ${p1.message}")
-                binding.progressBar.isVisible=false
-                binding.swipeRef.isRefreshing=false
-
+            override fun onFailure(call: Call<News>, t: Throwable) {
+                Log.e("MainActivity", "Error: ${t.message}")
+                binding.progressBar.isVisible = false
+                binding.swipeRef.isRefreshing = false
             }
         })
     }
-    private fun showNews(articles: ArrayList<Article>){
-        val adapter=NewsAdapter(this,articles)
-        binding.newsList.adapter=adapter
+
+
+    private fun setupAdapters() {
+
+        newsAdapter = NewsAdapter(this, mutableListOf())
+        binding.newsList.adapter = newsAdapter
+
+
+        categoriesAdapter = CategoriesAdapter(this, mutableListOf()) { selectedCategory ->
+            fetchNews(selectedCategory)
+            binding.categoriesList.layoutManager =
+                LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+
+        }
+
+        binding.categoriesList.adapter = categoriesAdapter
+
     }
+
+    private fun categoryNews(category: MutableList<Category>) {
+        val adapter = CategoriesAdapter(this, category) { selectedCategory ->
+            fetchNews(selectedCategory)
+        }
+        binding.categoriesList.adapter = adapter
+    }
+
+
 }
