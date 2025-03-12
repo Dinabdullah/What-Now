f4dcf1f31d955211ef3826c31fd4c78b23d868ca
HP <mariayousif318@gmail.com>
2025-03-12 21:05:43 +0200

    maria youssef

--- app/src/main/java/com/example/whatnow/ProfileActivity.kt
+++ app/src/main/java/com/example/whatnow/ProfileActivity.kt
@@ -9,16 +9,12 @@ class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // تفعيل View Binding
         binding = ActivityProfileBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        // استرجاع بيانات المستخدم
         val sharedPref = getSharedPreferences("UserData", MODE_PRIVATE)
         val username = sharedPref.getString("username", "User Name")
         val email = sharedPref.getString("email", "email@example.com")
 
-        // عرض البيانات في الواجهة
         binding.tvUsername.text = username
         binding.tvEmail.text = email
     }
